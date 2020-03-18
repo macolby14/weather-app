20b4ef60edc358eacdd24ae37328185a14fb1d44
Mark Colby <macolby14@gmail.com>
2020-03-17 21:44:57 -1000

    added Axios with OpenWeather and started results

--- src/components/containers/WeatherDisplay/WeatherDisplay.jsx
+++ src/components/containers/WeatherDisplay/WeatherDisplay.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import axios from "axios";
 
+import classes from "./WeatherDisplay.module.css";
 import Title from "../../Title/Title";
 import ZipCode from "../../ZipCode/ZipCode";
 import Button from "../../Button/Button";
-import classes from "./WeatherDisplay.module.css";
 
 class WeatherDisplay extends React.Component {
   state = {
     digits: [null, null, null, null, null],
-    activeDigitIndex: 0
+    activeDigitIndex: 0,
+    apiData: null
   };
+
+  componentDidMount() {
+    this.axiosInstance = axios.create({
+      baseURL: "https://api.openweathermap.org/data/2.5"
+    });
+  }
 
   digitChangedHandler = (e, digitIndex) => {
     const copyDigits = [...this.state.digits]; //avoid changing state directly
@@ -23,11 +31,21 @@ class WeatherDisplay extends React.Component {
     const searchZipCode = this.state.digits.join("");
     console.log("Button clicked - asking for weather in", searchZipCode);
     //will need to send to api and get back data
+    this.axiosInstance
+      .get("/weather?zip=96734,us&appid=")
+      .then(response => {
+        this.setState({ apiData: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+        alert("Error in searchClickHandler in WeatherDisplay.jsx");
+      });
   };
 
   render() {
-    return (
-      <div className={classes.WeatherDisplay}>
+    let displayFields = null;
+    let searchFields = (
+      <React.Fragment>
         <Title> What's the weather? </Title>
         <ZipCode
           digits={this.state.digits}
@@ -35,6 +53,18 @@ class WeatherDisplay extends React.Component {
           activeDigitIndex={this.state.activeDigitIndex}
         />
         <Button click={this.searchClickHandler}>Search</Button>
+      </React.Fragment>
+    );
+
+    if (this.state.apiData) {
+      searchFields = null;
+      displayFields = <Title>Here's the weather for you:</Title>;
+    }
+
+    return (
+      <div className={classes.WeatherDisplay}>
+        {searchFields}
+        {displayFields}
       </div>
     );
   }
