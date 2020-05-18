8d3eb3f57124e063fae3b1b0e516d74cdaefa6a4
Mark Colby <macolby14@gmail.com>
2020-05-18 11:56:27 -1000

    fix enter your zipcode text

--- src/components/ZipCode/ZipCode.jsx
+++ src/components/ZipCode/ZipCode.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Digit from "./Digit/Digit";
 import classes from "./ZipCode.module.css";
 
-const ZipCode = props => {
+const ZipCode = (props) => {
   const digits = props.digits.map((e, i) => {
     const shouldHaveFocus = props.activeDigitIndex === i;
 
@@ -11,7 +11,7 @@ const ZipCode = props => {
       <Digit
         key={i}
         value={e}
-        digitChanged={e => {
+        digitChanged={(e) => {
           props.digitChanged(e, i);
         }}
         hasFocus={shouldHaveFocus}
@@ -19,7 +19,12 @@ const ZipCode = props => {
     );
   });
 
-  return <div className={classes.ZipCode}>{digits}</div>;
+  return (
+    <div className={classes.ZipCode}>
+      <div className={classes.EnterText}>Enter your Zip Code</div>
+      {digits}
+    </div>
+  );
 };
 
 export default ZipCode;
